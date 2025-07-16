8c1609622e1eed07a6cfdbb2b5a81dd7a86ea6f6
roybiswajyoti1997 <42398275+roybiswajyoti1997@users.noreply.github.com>
2025-07-16 17:42:30 +0200

    Update main_client.cpp

--- tcp_benchmarking_test/main_client.cpp
+++ tcp_benchmarking_test/main_client.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "Connection.hpp"
+#include "DataProvider.hpp"
+#include "Benchmark.hpp"
+
+int main() {
+    const std::string serverIP = "141.76.47.8";  // change to localhost for local test
+    const int port = 8080;
+
+    Connection clientConn;
+    if (!clientConn.connectToServer(serverIP, port)) {
+        std::cerr << "[Client] Failed to connect to server.\n";
+        return 1;
+    }
+
+    // Generate 1MB dummy data
+    DataProvider dp;
+    dp.generateDummyData(1024 * 1024 / sizeof(uint64_t));  // 1MB = 1M bytes = (1M / 8) uint64_t
+
+    // Run throughput benchmark
+    Benchmark& bench = Benchmark::getInstance();
+    bench.runThroughput(clientConn, dp);
+
+    clientConn.closeConnection();
+    return 0;
+}
 
